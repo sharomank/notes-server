7f818290b40f260dd90be813d8391cfde6ae9de4
Roman Kurbangaliyev <roman_kurbangaliyev@epam.com>
2015-06-20 15:40:57 +0300

    Added Spring Boot with Actuator

---
+++ src/main/java/com/sharomank/progress/ProgressApplication.java
@@ -0,0 +1,11 @@
+package com.sharomank.progress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgressApplication {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ProgressApplication.class, args);
+    }
+}
