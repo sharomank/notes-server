0d5a7ddb8f9cfb78f391eb45df9b795589fd92cf
Roman Kurbangaliyev <roman_kurbangaliyev@epam.com>
2015-10-18 01:06:55 +0300

    fix for coverity-scan

--- src/main/java/com/sharomank/notes/util/JavaBeanUtils.java
+++ src/main/java/com/sharomank/notes/util/JavaBeanUtils.java
@@ -4,23 +4,21 @@ import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.util.ReflectionUtils;
 
-import static org.springframework.util.Assert.notNull;
-
 public final class JavaBeanUtils {
     private JavaBeanUtils() {
     }
 
-    public static void copyNotNullProperties(Object source, Object target) {
-        notNull(source);
-        notNull(target);
-        final BeanWrapper src = new BeanWrapperImpl(source);
-        final BeanWrapper trg = new BeanWrapperImpl(target);
-        ReflectionUtils.doWithFields(source.getClass(), field -> {
-            String propName = field.getName();
-            Object srcPropValue = src.getPropertyValue(propName);
-            if (srcPropValue != null) {
-                trg.setPropertyValue(propName, srcPropValue);
-            }
-        });
+    public static void copyNotNullProperties(final Object source, final Object target) {
+        if (source != null && target != null) {
+            final BeanWrapper src = new BeanWrapperImpl(source);
+            final BeanWrapper trg = new BeanWrapperImpl(target);
+            ReflectionUtils.doWithFields(source.getClass(), field -> {
+                String propName = field.getName();
+                Object srcPropValue = src.getPropertyValue(propName);
+                if (srcPropValue != null) {
+                    trg.setPropertyValue(propName, srcPropValue);
+                }
+            });
+        }
     }
 }
